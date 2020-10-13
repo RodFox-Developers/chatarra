48e145d39d90b91a52f1207241489d5328f74bf7
RodrigoCevas <rodfox.developers@gmail.com>
2020-10-13 19:54:10 +0200

    navbar components loading

--- scripts.js
+++ scripts.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
+    $('.discografia').click(function() {        
+        $('#loadData').load('discografia.html');
+    })
+    
+    $('.videos').click(function() {        
+        $('#loadData').load('videos.html');
+    })
+
+    $('.bio').click(function() {        
+        $('#loadData').load('biografia.html');
+    })
+
+    $('.gira').click(function() {        
+        $('#loadData').load('gira.html');
+    })
+
+    $('.contacto').click(function() {        
+        $('#loadData').load('contacto.html');
+    })
+
     $('.nav-item a').click(function() {
         $('.nav-item').removeClass('active');
         $(this).closest('.nav-item').addClass('active');
     })
+
+
 })
