e3270df2b47b8ed96e269f2d1bebf3c07cc08277
RodrigoCevas <rodfox.developers@gmail.com>
2020-11-03 16:52:03 +0100

    discografia bandcamp

--- scripts.js
+++ scripts.js
@@ -1,33 +1,57 @@
 $(document).ready(function() {
-    $('#loadData').load('inicio.html');
+    $('#loadData').load('inicio.html', function () {
+        $(window).scrollTop(0);
+    });
 
     $('.inicio').click(function() {        
-        $('#loadData').load('inicio.html');
+        $('#loadData').load('inicio.html', function () {
+            $(window).scrollTop(0);
+        });
+        
     })
 
     $('.discografia').click(function() {        
-        $('#loadData').load('discografia.html');
+        $('#loadData').load('discografia.html', function () {
+            $(window).scrollTop(0);
+        });
+        
     })
     
     $('.videos').click(function() {        
-        $('#loadData').load('videos.html');
+        $('#loadData').load('videos.html', function () {
+            $(window).scrollTop(0);
+        });
+        
     })
 
     $('.bio').click(function() {        
-        $('#loadData').load('biografia.html');
+        $('#loadData').load('biografia.html', function () {
+            $(window).scrollTop(0);
+        });
+        
     })
 
     $('.gira').click(function() {        
-        $('#loadData').load('gira.html');
+        $('#loadData').load('gira.html', function () {
+            $(window).scrollTop(0);
+        });
+        
     })
 
     $('.contacto').click(function() {        
-        $('#loadData').load('contacto.html');
+        $('#loadData').load('contacto.html', function () {
+            $(window).scrollTop(0);
+        });
+        
     })
 
     $('.nav-item a').click(function() {
         $('.nav-item').removeClass('active');
         $(this).closest('.nav-item').addClass('active');
-    })       
+    }) 
+    
+    $('.hide-toggle-navbar').on('click',function() {        
+        $('.navbar-collapse').collapse('hide');        
+    });
     
 })
